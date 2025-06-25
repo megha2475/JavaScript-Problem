6c60905a9ae82c28ba07c311ebd8020bf6efc066
megha2475 <meghaks2003@gmail.com>
2025-06-25 16:29:37 +0530

    Create descending-order.js

---
+++ Array/descending-order.js
@@ -0,0 +1,13 @@
+//Program to sort an array in descending order.
+
+let arr = [4,2,1,9,5,7];
+for(let i=0;i<arr.length;i++){
+    for(let j=i+1;j<arr.length;j++){
+        if(arr[i]<arr[j]){
+            let temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+ }
+ console.log(arr);
